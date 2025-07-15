3c0f70a3317271287aec6c92f0494ca24c9a9326
Violet-Tendencies <saphfiremusic@gmail.com>
2025-07-15 03:31:26 -0500

    Fixed it displaying the mod twice

--- UltraViolet/ultraviolet.lua
+++ UltraViolet/ultraviolet.lua
@@ -1,9 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: UltraViolet
---- MOD_ID: ULTRAVIOLET
---- MOD_AUTHOR: [Violet]
---- MOD_DESCRIPTION: Another assorted jokers mod (because we don't have enough!)\nIncludes Jokers inspired by Arknights, Ultrakill, Marvel Rivals, and more along with random jokers :3
---- PREFIX: uv
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
