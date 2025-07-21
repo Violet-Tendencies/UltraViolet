62087af442af461553e9fdcb806a73755c58d1d0
Violet-Tendencies <saphfiremusic@gmail.com>
2025-07-21 05:31:29 -0500

    Added 3 heroes

--- UltraViolet/data/jokers/rivals.lua
+++ UltraViolet/data/jokers/rivals.lua
@@ -78,12 +78,12 @@ SMODS.Joker{
     end
 }
 
---[[ SMODS.Joker{
+SMODS.Joker{
     key = "winter_soldier",
     loc_txt = {
         name = "Winter Soldier",
         text = {
-            'Scores {C:red}+3{} mult 3 times',
+            'Scores {C:red}+#1#{} mult #2# times',
             '{C:inactive}Fully Armed.{}',
             '{C:inactive}(Marvel Rivals){}'
         }
@@ -99,10 +99,143 @@ SMODS.Joker{
     pos = {x=0, y=0},
     config = {
         extra = {
-            mult = 3
+            mult = 3,
+            chips = 0,
+            retriggers = 2,
+            retUp = false
         }
     },
+    loc_vars = function(self, info_queue, card)
+        return { vars = {card.ability.extra.mult, card.ability.extra.retriggers} }
+    end,
     calculate = function(self, card, context)
-        
+        if context.setting_blind then
+            juice_card(card)
+            G.GAME.blind.chips = G.GAME.blind.chips - (G.GAME.blind.chips * 0.15)
+            G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+            card.ability.extra.retUp = false
+        end
+        if context.joker_main then
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+        if context.retrigger_joker_check then
+            return {
+                repetitions = card.ability.extra.retriggers,
+                card = card
+            }
+        end
+        if context.end_of_round then
+            if not card.ability.extra.retUp then
+                card.ability.extra.retriggers = card.ability.extra.retriggers + 1
+                card.ability.extra.retUp = true
+            end
+        end
     end
-} ]]
+}
+
+SMODS.Joker{
+    key = "mantis",
+    loc_txt = {
+        name = "Mantis",
+        text = {
+            'Scores +#2# chips per each other joker held',
+            'This joker gains +#1# mult every hand',
+            '{C:inactive}(Currently #1#){}',
+            '{C:inactive}Life always wins!{}',
+            '{C:inactive}(Marvel Rivals){}'
+        }
+    },
+    atlas = "RivalsJokers",
+    rarity = 2,
+    cost = 4,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = false,
+    pos = {x=0, y=0},
+    config = {
+        extra = {
+            mult = 2,
+            chips = 1,
+            multUp = false
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = {card.ability.extra.mult, card.ability.extra.chips } }
+    end,
+    calculate = function(self, card, context)
+        if context.before then
+            card.ability.extra.multUp = false
+        end
+        if context.post_trigger then
+            card.ability.extra.chips = card.ability.extra.chips + (#G.jokers.cards - 1)
+            juice_card(card)
+        end
+        if context.joker_main then
+            if not card.ability.extra.multUp then
+                juice_card(card)
+                card.ability.extra.mult = card.ability.extra.mult + 2
+                card.ability.extra.multUp = true
+            end
+            return {
+                mult = card.ability.extra.mult,
+                chips = card.ability.extra.chips * (#G.jokers.cards - 1)
+            }
+        end
+    end
+}
+
+SMODS.Joker{
+    key = "hela",
+    loc_txt = {
+        name = "Hela",
+        text = {
+            'Scores +#1# chips each hand',
+            'On the final hand, score +#2#',
+            'per each hand played this blind',
+            '{C:inactive}Hel\'s blades are sharp indeed.{}',
+            '{C:inactive}(Marvel Rivals){}'
+        }
+    },
+    atlas = "RivalsJokers",
+    rarity = 2,
+    cost = 4,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = false,
+    pos = {x=0, y=0},
+    config = {
+        extra = {
+            finalChips = 30,
+            normalChips = 10,
+            helChips = 250
+        }
+    },
+    loc_vars = function(self, info_queue, card)
+        return { vars = {card.ability.extra.mult, card.ability.extra.mult } }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            if context.blind.boss and (G.GAME.current_round.hands_left + 1) % 2 == 0 then
+                return {
+                    chips = card.ability.extra.helChips,
+                    message = "A feast for my crows!",
+                    card = card
+                }
+            elseif G.GAME.current_round.hands_left <= 0 then
+                return {
+                    chips = card.ability.extra.finalChips * G.GAME.current_round.hands_played
+                }
+            else
+                return {
+                    chips = card.ability.extra.normalChips
+                }
+            end
+        end
+    end
+}
