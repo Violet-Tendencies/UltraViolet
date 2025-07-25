acafea66f02524ee5311b89e782d9d22fa875f06
Violet-Tendencies <saphfiremusic@gmail.com>
2025-07-25 06:50:04 -0500

    Adding the Stake (WIP)

--- ultraviolet.lua
+++ ultraviolet.lua
@@ -5,6 +5,7 @@ SMODS.load_file("data/jokers/rivals.lua")()
 SMODS.load_file("data/jokers/stupendium.lua")()
 SMODS.load_file("data/jokers/religions/sins.lua")()
 SMODS.load_file("data/blinds/religions/virtues.lua")()
+SMODS.load_file("data/stakes/insanity.lua")()
 
 ----------------------------------------------
 ------------Other stuff-----------------------
