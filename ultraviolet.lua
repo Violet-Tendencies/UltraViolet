283b5db219e1ffac1d9f156ebcfec7dc9a39a9d2
Violet-Tendencies <saphfiremusic@gmail.com>
2025-08-13 15:38:48 -0500

    Update ultraviolet.lua

--- ultraviolet.lua
+++ ultraviolet.lua
@@ -1,11 +1,12 @@
 ----------------------------------------------
 -------------- Files -------------------------
 
-SMODS.load_file("data/jokers/rivals.lua")()
-SMODS.load_file("data/jokers/stupendium.lua")()
-SMODS.load_file("data/jokers/religions/sins.lua")()
-SMODS.load_file("data/blinds/religions/virtues.lua")()
-SMODS.load_file("data/stakes/insanity.lua")()
+assert(SMODS.load_file("data/jokers/rivals.lua"))()
+assert(SMODS.load_file("data/jokers/stupendium.lua"))()
+assert(SMODS.load_file("data/jokers/religions/sins.lua"))()
+assert(SMODS.load_file("data/blinds/religions/virtues.lua"))()
+assert(SMODS.load_file("data/stakes/insanity.lua"))()
+assert(SMODS.load_file("data/Technically Cross-Mod stuff/oaths.lua"))()
 
 ----------------------------------------------
 ------------Other stuff-----------------------
